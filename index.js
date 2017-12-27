554400f59df758e960aefb4264d4d148b4744ee9
David McGrann <dpmcgrann@gmail.com>
2017-12-27 21:03:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ var recipes = {}
 function updateObjectWithKeyAndValue(object, key, value) {
   return {}
 }
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return ({}, recipes, { prop: '1', prop2: '2'})
+}
