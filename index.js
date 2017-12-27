f09f06c470b4500e61dbdca35a77d117c5815681
David McGrann <dpmcgrann@gmail.com>
2017-12-27 21:07:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ function updateObjectWithKeyAndValue(object, key, value) {
 function updateObjectWithKeyAndValue(object, key, value) {
   return ({}, recipes, { prop: '1', prop2: '2'})
 }
+
+function destructivelyUpdaateObjectKeyAndValue(object, key, value) {
+  object[key] = value
+  return {}
+}
